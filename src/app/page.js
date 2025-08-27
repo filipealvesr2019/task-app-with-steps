28d9bff9e9ace46acc5de1bd770103734c2850b7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-27 20:47:21 -0300

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import TasksPage from "@/Pages/Task";
+import TasksPage from "@/app/Task";
 
 export default function Home() {
   return (
